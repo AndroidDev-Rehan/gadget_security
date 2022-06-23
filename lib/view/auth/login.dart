dcb9b12ea928ae4051e18c3a89d47f5a3868e769
Saif <saifr383@gmail.com>
2022-06-23 10:11:50 -0700

    signin and up

---
+++ lib/view/auth/login.dart
@@ -0,0 +1,219 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../../utils/color.dart';
+
+
+class LoginPage extends StatelessWidget {
+  LoginPage({Key? key}) : super(key: key);
+
+  final GlobalKey<FormState> _loginFormKey = GlobalKey<FormState>();
+
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: OrientationBuilder(
+
+          builder: (context, snapshot) {
+            return Scaffold(
+              body: SingleChildScrollView(
+                child: SizedBox(
+                  width: Get.width,
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      children:  [
+                        const SizedBox(height: 20,),
+                        Image.asset("assets/logo.jpg", scale: 3,),
+                        const SizedBox(height: 20,),
+                        signInContainer(),
+                        const SizedBox(height: 20,),
+                        belowContainer(),
+
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            );
+          }
+      ),
+    );
+  }
+
+  signInContainer(){
+    return Material(
+      elevation: 10,
+      borderRadius: BorderRadius.circular(20),
+      child: Container(
+        padding: const EdgeInsets.fromLTRB(16, 24, 16, 16),
+        decoration:  BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(20),
+
+        ),
+        child: SizedBox(
+          width: double.infinity,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Row(
+                children: const [
+                  Text("Sign in", style: TextStyle(fontSize: 22, fontWeight: FontWeight.w600),),
+                ],
+              ),
+              const SizedBox(height: 7,),
+              Row(
+                children: const [
+                  Text("to continue", style:TextStyle(fontSize: 22, fontWeight: FontWeight.w600)),
+                ],
+              ),
+              const SizedBox(height: 20,),
+              textFieldsColumn(),
+              const SizedBox(height: 30,),
+              forgotPassword(),
+              const SizedBox(height: 20,),
+              loginButton("Login"),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  textFieldsColumn(){
+
+    return Form(
+      key: _loginFormKey,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          customTextField("Enter Email", TextEditingController(), Icons.person,
+                  (String? value){
+                if (value == null) {
+                  return 'Email is required';
+                }
+                if (value.isEmpty) {
+                  return 'Email is required';
+                }
+
+                return null;
+              }
+          ),
+          customTextField("Enter Password", TextEditingController(), Icons.lock,
+                  (String? value) {
+                if (value==null) {
+                  return 'password is required';
+                }
+                if(value.isEmpty){
+                  return 'password is required';
+                }
+                return null;
+              }
+
+          ),
+        ],
+      ),
+    );
+  }
+
+  customTextField(String hint,TextEditingController controller, IconData prefixIcon, FormFieldValidator<String?> validator ){
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 8.0),
+      child: TextFormField(
+        controller: controller,
+        validator: validator,
+        decoration: InputDecoration(
+          hintText: hint,
+          filled: true,
+          border: InputBorder.none,
+//          prefix: Icon(prefixIcon),
+          prefixIcon: Icon(prefixIcon),
+        ),
+      ),
+    );
+  }
+
+  loginButton(String text){
+
+    return Container(
+      width: double.infinity,
+      child: ElevatedButton(
+
+          onPressed: (){
+            if(_loginFormKey.currentState!.validate()) {
+            }
+          },
+          child: Text(text, style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 18),),
+          style: ElevatedButton.styleFrom(
+            primary: ColorResources.COLOR_PRIMARY,
+
+            fixedSize: const Size(double.infinity, 50),
+          )
+      ),
+    );
+  }
+
+  forgotPassword(){
+    return GestureDetector(
+      onTap: (){},
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        children: const [
+          Text("Forgot Password?", style: TextStyle(color: ColorResources.COLOR_PRIMARY, fontWeight: FontWeight.bold, fontSize: 16),),
+        ],
+      ),
+    );
+  }
+
+  belowContainer(){
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        SizedBox(
+          width: double.infinity,
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("Don't have an account? ", style: TextStyle(color: Colors.black.withOpacity(0.7)),),
+              InkWell(
+                  onTap: (){
+
+                  },
+                  child: Text("Create new account", style: TextStyle(color: Colors.purple, fontWeight: FontWeight.w600),)),
+            ],
+          ),
+        ),
+        const SizedBox(height: 20,),
+        Row(
+          children: [
+            Expanded(
+              child: Container(
+                  width: double.infinity,
+                  height: 1,
+                  color: Colors.grey.withOpacity(0.2)
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 15),
+              child: Text("Or"),
+            ),
+            Expanded(
+              child: Container(
+                  width: double.infinity,
+                  height: 1,
+                  color: Colors.grey.withOpacity(0.2)
+              ),
+            )                ],
+        ),
+        const SizedBox(height: 20,),
+
+      ],
+    );
+  }
+
+
+
+}
---
+++ lib/view/auth/signup.dart
@@ -0,0 +1,200 @@
+import 'dart:developer';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+
+class SignUpScreen extends StatelessWidget {
+
+  SignUpScreen({Key? key}) : super(key: key);
+
+  final GlobalKey<FormState> _signUpFormKey = GlobalKey<FormState>();
+
+
+  @override
+  Widget build(BuildContext context) {
+    log('/////////////////////////////////');
+
+    return SafeArea(
+      child: OrientationBuilder(
+          builder: (context, snapshot) {
+            return Scaffold(
+              body: SingleChildScrollView(
+                child: SizedBox(
+                  width: Get.width,
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      children:  [
+                        const SizedBox(height: 20,),
+                        Image.asset("assets/images/logo.png", scale: 3,),
+                        const SizedBox(height: 20,),
+                        signInContainer(),
+                        const SizedBox(height: 20,),
+                        SizedBox(
+                          width: double.infinity,
+                          child: Row(
+                            mainAxisSize: MainAxisSize.max,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text("I already have an account.  ", style: TextStyle(color: Colors.black.withOpacity(0.7)),),
+                              InkWell(
+                                  onTap: (){
+
+                                  },
+                                  child: const Text("Sign In", style: TextStyle(color: Colors.purple, fontWeight: FontWeight.w600),)),
+                            ],
+                          ),
+                        )
+//                  belowContainer(),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            );
+          }
+      ),
+    );
+  }
+
+  signInContainer(){
+
+
+    return Material(
+      elevation: 10,
+      borderRadius: BorderRadius.circular(20),
+      child: Container(
+        padding: const EdgeInsets.fromLTRB(16, 24, 16, 16),
+        decoration:  BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(20),
+
+        ),
+        child: SizedBox(
+          width: double.infinity,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Row(
+                children: const [
+                  Text("Create New", style: TextStyle(fontSize: 22, fontWeight: FontWeight.w600),),
+                ],
+              ),
+              const SizedBox(height: 7,),
+              Row(
+                children: const [
+                  Text("Account", style:TextStyle(fontSize: 22, fontWeight: FontWeight.w600)),
+                ],
+              ),
+              const SizedBox(height: 20,),
+              textFieldsColumn(),
+//              const SizedBox(height: 10,),
+              const SizedBox(height: 30,),
+              signUpButton("Create new Account")
+//              forgotPassword(),
+//              loginButton("Login"),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  textFieldsColumn(){
+    return Form(
+      key: _signUpFormKey,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          customTextField2("Enter Email", TextEditingController() , Icons.email_outlined,
+                  (String? value){
+                if (value == null) {
+                  return 'Email is required';
+                }
+                if (value.isEmpty) {
+                  return 'Email is required';
+                }
+                if (!GetUtils.isEmail(value)) {
+                  return "Invalid Format";
+                }
+                return null;
+              }
+          ),
+          customTextField2("Enter UserName", TextEditingController(), Icons.person,
+                (String? value){
+              if (value == null) {
+                return 'User Name is required';
+              }
+              return null;
+            },
+          ),
+          customTextField2("Enter Phone Number", TextEditingController(), Icons.phone,
+                  (String? value){
+                if (value == null) {
+                  return 'Phone Number is required';
+                }
+                if (value.length < 11) {
+                  return 'Invalid Number';
+                }
+                return null;
+              },keyboardType: TextInputType.number
+          ),
+          customTextField2("Enter Password",TextEditingController(), Icons.lock,
+                  (String? value) {
+                if (value==null) {
+                  return 'password is required';
+                }
+                if(value.isEmpty){
+                  return 'password is required';
+                }
+                return null;
+              }
+
+          ),
+
+        ],
+      ),
+    );
+  }
+
+  customTextField2(String hint,TextEditingController controller, IconData prefixIcon, FormFieldValidator<String?> validator, {TextInputType? keyboardType} ){
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 8.0),
+      child: TextFormField(
+        keyboardType: keyboardType ?? TextInputType.text,
+        controller: controller,
+        validator: validator,
+        decoration: InputDecoration(
+          hintText: hint,
+          filled: true,
+          border: InputBorder.none,
+//          prefix: Icon(prefixIcon),
+          prefixIcon: Icon(prefixIcon),
+        ),
+      ),
+    );
+  }
+
+
+
+  signUpButton(String text){
+
+
+    return SizedBox(
+      width: double.infinity,
+      child: ElevatedButton(
+          onPressed: (){
+
+            if(_signUpFormKey.currentState!.validate() ) {
+
+            }
+          },
+          child: Text(text, style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 18),),
+          style: ElevatedButton.styleFrom(
+            fixedSize: const Size(double.infinity, 50),
+          )
+      ),
+    );
+  }
+
+}
